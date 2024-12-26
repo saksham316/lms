555e02a6c8cdf8dd83cfcbf0e2a1f86e151138b7
saksham <phantomdx316@gmail.com>
2024-12-26 08:50:05 +0530

    added

--- api/src/controllers/AuthControllers/AdminController/adminAuth.js
+++ api/src/controllers/AuthControllers/AdminController/adminAuth.js
@@ -15,35 +15,32 @@ export const loginAdmin = async (req, res) => {
   try {
     // console.log(chalk.bgRedBright(process.env.ACCESS_TOKEN_VALIDITY));
     // console.log(os);
-    console.log("1");
     const jwt = jsonwebtoken;
     const { loginSession } = req;
     const { email, password } = req?.body;
     const { userExistence } = req?.body;
     // console.log("THis is userExistence",userExistence)
-    console.log("2");
     if (userExistence?.disabled) {
       return res.status(400).json({
         success: false,
         message: "Unauthorized Access",
       });
     }
-    console.log("3");
     const passwordConfirmation = await bcrypt.compare(
       password,
       userExistence?.password
-      );
-      console.log("4");
-      // console.log(chalk.bgCyan("This is incioming password", password));
-      // console.log(
-        //   chalk.bgGreen("This is hashed password", userExistence?.password)
-        // );
+    );
+    // console.log(chalk.bgCyan("This is incioming password", password));
+    // console.log(
+    //   chalk.bgGreen("This is hashed password", userExistence?.password)
+    // );
     if (!passwordConfirmation) {
       return res.status(400).json({
         success: false,
         message: "Wrong Password",
       });
     }
+    console.log("4");
     // accessToken - Generating Access Token
     const accessToken = jwt.sign(
       {
@@ -52,18 +49,22 @@ export const loginAdmin = async (req, res) => {
       process.env.JWT_SECRET_KEY,
       { expiresIn: process.env.ACCESS_TOKEN_VALIDITY }
     );
+    console.log("3");
 
     // console.log(userExistence.id);
     // Saving accessToken to the httpOnly Cookie
     saveAccessTokenToCookie(res, accessToken);
+    console.log("2");
+
     saveIndividualLoginDetails(res, loginSession);
     // console.log("this is user id", accessToken);
+    console.log("1");
 
     let deciphered = CryptoJS.AES.encrypt(
       JSON.stringify(userExistence),
       process.env.CRYPTO_SECRET_KEY
     ).toString();
-    console.log("entered here")
+    console.log("entered here");
 
     return res.status(200).json({
       success: true,
@@ -71,12 +72,7 @@ export const loginAdmin = async (req, res) => {
       user: deciphered,
     });
   } catch (error) {
-    console.log(
-      chalk.bgRedBright(
-        "Inside catch block of login:::::",
-        error
-      )
-    );
+    console.log(chalk.bgRedBright("Inside catch block of login:::::", error));
     return res.status(400).json({
       success: false,
       message: error?.message || error,
